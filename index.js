6081373941551b47226496a058012529c0332b28
ronny332 <ronny332@gmx.de>
2017-11-02 13:10:42 +0100

    first usable version (cmi reading only, no writing)

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
 global.__base = __dirname + '/';
 
 require(__base + 'lib/config');
-require(__base + "lib/cmi.server");
-require(__base + "lib/cmi.message");
-const cmiData = require(__base + "lib/cmi.data");
+require(__base + 'lib/cmi.server');
+require(__base + 'lib/cmi.message');
+const cmiData = require(__base + 'lib/cmi.data');
+require(__base + 'lib/fhem.telnet')(cmiData.data);
 
-setInterval(() => {
-  console.log(cmiData.get(55, 'D'));
-  console.log(cmiData.get(56, 'A'));
-}, 10000);
+// setInterval(() => {
+//   console.log(cmiData.get(55, 'D'));
+//   console.log(cmiData.get(56, 'A'));
+// }, 10000);
