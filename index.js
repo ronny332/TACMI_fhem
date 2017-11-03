b4689649f4e24ab26d00aa6ca094224010e7f728
ronny332 <ronny332@gmx.de>
2017-11-03 11:18:59 +0100

    added states functionality (save current states after fixed interval, read last states after restart; from json file)

--- index.js
+++ index.js
@@ -3,10 +3,13 @@
 global.__base = __dirname + '/';
 
 require(__base + 'lib/config');
+require(__base + 'lib/init');
 require(__base + 'lib/cmi.server');
 require(__base + 'lib/cmi.message');
 const cmiData = require(__base + 'lib/cmi.data');
 require(__base + 'lib/fhem.telnet')(cmiData.data);
+require(__base + 'lib/states.load')(cmiData.data);
+require(__base + 'lib/states.store')(cmiData.data);
 
 // setInterval(() => {
 //   console.log(cmiData.get(55, 'D'));
