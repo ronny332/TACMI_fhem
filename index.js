d31f8dfde1d76ee69bd10f49e3eed87d8bacf2fd
ronny332 <ronny332@gmx.de>
2017-11-11 13:15:50 +0100

    first version with write/set support. for now values will be sent by the cmi.udp.client.cron, which sends the data after a configured interval. immediate sending will follow shortly.

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ global.__base = __dirname + '/';
 
 require(__base + 'lib/config');
 require(__base + 'lib/init');
-require(__base + 'lib/cmi.server');
+require(__base + 'lib/cmi.udp.client');
+require(__base + 'lib/cmi.udp.server');
 require(__base + 'lib/cmi.message');
 const cmiData = require(__base + 'lib/cmi.data'),
-  client = require(__base + 'lib/fhem.telnet.client')(cmiData.data);
-require(__base + 'lib/fhem.telnet.server')(cmiData.data, client);
-require(__base + 'lib/states.load')(cmiData.data);
-require(__base + 'lib/states.store')(cmiData.data);
+  client = require(__base + 'lib/fhem.telnet.client')(cmiData);
+require(__base + 'lib/fhem.telnet.server')(cmiData, client);
+require(__base + 'lib/states.load')(cmiData);
+require(__base + 'lib/states.store')(cmiData);
+require(__base + 'lib/cmi.udp.client.cron')(cmiData);
 
 // setInterval(() => {
 //   console.log(cmiData.get(55, 'D'));
